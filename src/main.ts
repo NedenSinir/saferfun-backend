42bd68a1a5ac5251d58f32e480345bcb5f7314c2
NedenSinir <123alperozyurt@gmail.com>
2024-10-08 14:42:24 +0200

    Refactor CORS configuration to allow requests from any origin

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: true, // Allows all origins
+    origin: (origin, callback) => {
+      // Allow requests from any origin
+      callback(null, true);
+    },
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
